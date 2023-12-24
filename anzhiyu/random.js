429d4b671b55eca7bb8aebd6f82a16442a91b3cc
mozii-org <mamaipee@outlook.com>
2023-12-24 20:04:21 +0800

    Site updated: 2023-12-24 20:04:19

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/19/搭建Signal-Server/"];function toRandomPost(){
+var posts=["2023/12/24/使用命令行工具破解PDF文件使其可编辑/","2023/12/19/搭建Signal-Server/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
